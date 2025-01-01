725c06b941505f9a1ef75f2c39e729ace51195fb
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-01-01 17:42:28 +0530

    chore: add assets and starter code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,33 +8,28 @@ export default {
     },
 
     fontFamily: {
-      body: ["Roboto", "sans-serif"],
+      body: ["JetBrains-Mono", "monospace"],
     },
 
     fontSize: {
-      "heading-desktop": [
-        "3.5rem",
+      "preset-1": [
+        "2rem",
         { lineHeight: "1", letterSpacing: "0px", fontWeight: "700" },
       ],
 
-      "heading-mobile": [
-        "2.5rem",
+      "preset-2": [
+        "1.5rem",
         { lineHeight: "1", letterSpacing: "0px", fontWeight: "700" },
       ],
 
-      body: [
-        "1rem",
-        { lineHeight: "1.5", letterSpacing: "0px", fontWeight: "400" },
+      "preset-3": [
+        "1.125rem",
+        { lineHeight: "1", letterSpacing: "0px", fontWeight: "700" },
       ],
 
-      "body-bold": [
+      "preset-4": [
         "1rem",
-        { lineHeight: "1.5", letterSpacing: "0px", fontWeight: "700" },
-      ],
-
-      "body-small": [
-        "0.75rem",
-        { lineHeight: "1.5", letterSpacing: "0px", fontWeight: "700" },
+        { lineHeight: "1", letterSpacing: "0px", fontWeight: "700" },
       ],
     },
 
@@ -42,29 +37,19 @@ export default {
       transparent: "transparent",
       white: "hsl(0, 0%, 100%)",
       black: "hsl(0, 0%, 0%)",
-      green: "hsl(159, 69%, 38%)",
 
-      red: {
-        DEFAULT: "hsl(14, 86%, 42%)",
-        hover: "hsl(14, 86%, 31%)",
-      },
-
-      rose: {
-        50: "hsl(20, 50%, 98%)",
-        100: "hsl(13, 31%, 94%)",
-        300: "hsl(14, 25%, 72%)",
-        400: "hsl(7, 20%, 60%)",
-        500: "hsl(12, 20%, 44%)",
-        900: "hsl(14, 65%, 9%)",
-      },
+      "almost-black": "hsl(248, 18%, 9%)",
+      "very-dark-grey": "hsl(248, 15%, 11%)",
+      "dark-grey": "hsl(247, 11%, 15%)",
+      grey: "hsl(251, 9%, 53%)",
+      "almost-white": "hsl(252, 11%, 91%)",
+      "neon-green": "hsl(127, 100%, 82%)",
+      yellow: "hsl(42, 91%, 68%)",
+      orange: "hsl(13, 95%, 66%)",
+      red: "hsl(0, 91%, 63%)",
     },
 
-    extend: {
-      gridTemplateColumns: {
-        "3fr-2fr": "3fr 2fr",
-        list: "auto 1fr",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 

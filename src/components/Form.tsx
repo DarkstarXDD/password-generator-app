182632c1835ac9448139267d3a4b1c62148d4f64
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-01-03 22:18:46 +0530

    feat: add password generation function

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,28 +5,62 @@ import Checkbox from "./Checkbox"
 import Button from "./Button"
 import { LiaArrowRightSolid } from "react-icons/lia"
 
-export default function Form() {
-  function generatePassword(event: React.FormEvent<HTMLFormElement>) {
-    const formData = new FormData(event.currentTarget)
-    const passwordLength = formData.get("password-length")
-    const passwordOptions = formData.getAll("password-options")
+function generatePassword(length: number, options: string[]): string {
+  let password = ""
 
-    console.log({
-      passwordLength: passwordLength,
-      passwordOptions: passwordOptions,
-    })
+  const charGroups = {
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    numbers: "0123456789",
+    symbols: "!@#$%^&*_-+()[]{}<>;:,.?",
+  }
+
+  let availableChars = ""
+
+  if (options.includes("uppercase")) {
+    availableChars += charGroups.uppercase
+  }
+
+  if (options.includes("lowercase")) {
+    availableChars += charGroups.lowercase
+  }
+
+  if (options.includes("numbers")) {
+    availableChars += charGroups.numbers
+  }
+
+  if (options.includes("symbols")) {
+    availableChars += charGroups.symbols
+  }
+
+  for (let i = 0; i < length; i++) {
+    const randomNumber = Math.floor(Math.random() * availableChars.length)
+    const randomChar = availableChars[randomNumber]
+    password += randomChar
+  }
+
+  return password
+}
+
+export default function Form() {
+  function handleChange(event: React.FormEvent<HTMLFormElement>) {
+    const formData = new FormData(event.currentTarget)
+    const passwordLength = Number(formData.get("password-length"))
+    const passwordOptions = formData.getAll("password-options") as string[]
+
+    console.log(generatePassword(passwordLength, passwordOptions))
   }
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
-    generatePassword(event)
+    handleChange(event)
   }
 
   return (
     <form
       className="grid gap-8 bg-dark-grey p-4"
       onSubmit={handleSubmit}
-      onChange={generatePassword}
+      onChange={handleChange}
     >
       <Slider name="password-length" min={8} max={32} defaultValue={12} />
 

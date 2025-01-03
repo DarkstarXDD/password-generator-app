34210e47073b61f37c24b454ada33bdf70f6fb68
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-01-03 08:32:08 +0530

    refactor: update button to receive classnames

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,14 +2,16 @@ import React, { ReactNode } from "react"
 
 export default function Button({
   children,
+  classNames,
   handleClick,
 }: {
   children?: ReactNode
+  classNames?: string
   handleClick?: React.MouseEventHandler<HTMLButtonElement>
 }) {
   return (
     <button
-      className="flex w-full items-center justify-center gap-6 border-2 border-neon-green bg-neon-green p-5 text-dark-grey transition-all hover:bg-dark-grey hover:text-neon-green"
+      className={`flex w-full items-center justify-center gap-6 border-2 border-neon-green bg-neon-green p-5 text-dark-grey transition-all hover:bg-dark-grey hover:text-neon-green ${classNames}`}
       onClick={handleClick}
     >
       {children}

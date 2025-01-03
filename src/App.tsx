1d19b5f6cfc9db47fbe9462b814e662a23fbdb5a
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-01-03 14:30:01 +0530

    feat: populate App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import Button from "./components/Button"
-import { LiaArrowRightSolid } from "react-icons/lia"
+import { useState } from "react"
+
+import CopyButton from "./components/CopyButton"
+import Form from "./components/Form"
 
 export default function App() {
+  const [password, setPassword] = useState("PTx1f5DaFX")
+
   return (
     <main>
-      <h1 className="text-3xl">Hello, World!</h1>
-      <Button>
-        Generate
-        <LiaArrowRightSolid strokeWidth={2} />
-      </Button>
+      <div className="grid max-w-xl gap-4">
+        <h1 className="text-3xl text-center text-grey">Password Generator</h1>
+
+        <div className="flex items-center justify-between bg-dark-grey p-5">
+          <p className="text-preset-2">{password}</p>
+          <CopyButton handleClick={() => console.log("Copy Clicked!")} />
+        </div>
+
+        <Form />
+      </div>
     </main>
   )
 }

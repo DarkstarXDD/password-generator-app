5f59ff750c9d6dd97dc559a719308bfe37095a0f
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-01-04 18:09:27 +0530

    refactor: update Checkbox id

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -5,17 +5,11 @@ function Checkbox({
   ...props
 }: { label: string } & ComponentProps<"input">) {
   const id = useId()
-  const checkboxId = `${id}-checkbox`
 
   return (
     <div className="flex items-center gap-5">
-      <input
-        type="checkbox"
-        id={checkboxId}
-        {...props}
-        className="cursor-pointer"
-      />
-      <label htmlFor={checkboxId}>{label}</label>
+      <input type="checkbox" id={id} {...props} className="cursor-pointer" />
+      <label htmlFor={id}>{label}</label>
     </div>
   )
 }
